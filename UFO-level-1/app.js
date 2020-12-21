919774a940bc1191027daf22a4d489e51179b696
NPVoravong <npvoravong@gmail.com>
2020-12-21 13:24:46 -0500

    Final Code Clean Up

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,31 +1,31 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
 // YOUR CODE HERE!
 
-data.forEach((item) => {
-  console.log(item);
-  let tr = d3.select("tbody").append("tr");
-  let datetime = tr.append("td").text(item.datetime);
-  let city = tr.append("td").text(item.city);
-  let state = tr.append("td").text(item.state);
-  let country = tr.append("td").text(item.country);
-  let shape = tr.append("td").text(item.shape);
-  let durationMinutes = tr.append("td").text(item.durationMinutes);
-  let comments = tr.append("td").text(item.comments);
-});
+runDefault();
 
-var button_filter = d3.select("#filter-btn");
-var button_reset = d3.select("#reset-btn");
-var form = d3.select("#form");
+function runDefault() {
+  d3.select("#ufo-tbody").html("");
+  data.forEach((item) => {
+    console.log(item);
+    let tr = d3.select("tbody").append("tr");
+    let datetime = tr.append("td").text(item.datetime);
+    let city = tr.append("td").text(item.city);
+    let state = tr.append("td").text(item.state);
+    let country = tr.append("td").text(item.country);
+    let shape = tr.append("td").text(item.shape);
+    let durationMinutes = tr.append("td").text(item.durationMinutes);
+    let comments = tr.append("td").text(item.comments);
+  })
+}
 
-button_filter.on("click", runFilter);
-button_reset.on("click", runReset);
-form.on("submit", runFilter);
+const button_filter = d3.select("#filter-btn").on("click", runFilter);
+const button_reset = d3.select("#reset-btn").on("click", runDefault);
+const form = d3.select("#form").on("submit", runFilter);
 
 function runFilter() {
-
-  document.getElementById("ufo-tbody").innerHTML = "";
+  d3.select("#ufo-tbody").html("");
 
   let user_input = d3.selectAll(".form-control");
   let input_val = user_input.property("value");
@@ -43,21 +43,5 @@ function runFilter() {
     let shape = tr.append("td").text(item.shape);
     let durationMinutes = tr.append("td").text(item.durationMinutes);
     let comments = tr.append("td").text(item.comments);
-  });
-}
-
-function runReset() {
-  document.getElementById("ufo-tbody").innerHTML = "";
-
-  data.forEach((item) => {
-    console.log(item);
-    let tr = d3.select("#ufo-table>tbody").append("tr");
-    let datetime = tr.append("td").text(item.datetime);
-    let city = tr.append("td").text(item.city);
-    let state = tr.append("td").text(item.state);
-    let country = tr.append("td").text(item.country);
-    let shape = tr.append("td").text(item.shape);
-    let durationMinutes = tr.append("td").text(item.durationMinutes);
-    let comments = tr.append("td").text(item.comments);
-  });
+  })
 }
